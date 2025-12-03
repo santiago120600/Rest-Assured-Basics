27944c466fbc736f8e9143958cd6df3d2054cf7e
Santiago Castañón <santiagocastanonarvizu@gmail.com>
2025-12-02 19:41:14 -0600

    feat: add ApiSpecTest

---
+++ src/test/java/com/restassured/tests/ApiSpecTest.java
@@ -0,0 +1,83 @@
+package com.restassured.tests;
+
+import static org.hamcrest.Matchers.notNullValue;
+
+import org.testng.annotations.BeforeGroups;
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
+
+public final class ApiSpecTest {
+
+    /*
+     * GET http://host.docker.internal/api/v1/authors HTTP/1.1
+     * Accept: application/json
+     */
+
+    public static RequestSpecification requestSpecificationExample() {
+        return new RequestSpecBuilder()
+                .setBaseUri("http://host.docker.internal/api/v1")
+                .setContentType(ContentType.JSON)
+                .addFilter(new RequestLoggingFilter())
+                .addFilter(new ResponseLoggingFilter())
+                .build();
+    }
+
+    @Test
+    public void testGetRequest() {
+        RestAssured.given(requestSpecificationExample())
+                .when()
+                .get("/authors")
+                .then()
+                .assertThat()
+                .statusCode(200);
+    }
+
+    public static ResponseSpecification responseSpecificationExample() {
+        return new ResponseSpecBuilder()
+                .expectStatusCode(200)
+                .expectContentType(ContentType.JSON)
+                .expectBody(notNullValue())
+                .build();
+    }
+
+    @Test
+    public void testGetRequest1() {
+        RestAssured.given()
+                .spec(requestSpecificationExample())
+                .when()
+                .get("/authors")
+                .then()
+                .spec(responseSpecificationExample());
+    }
+
+    public static RequestSpecification spec;
+    private static String baseUrl = "http://host.docker.internal/api/v1";
+
+    @BeforeGroups("specs")
+    public void setup() {
+        spec = new RequestSpecBuilder()
+                .setBaseUri(baseUrl)
+                .setContentType(ContentType.JSON)
+                .addFilter(new RequestLoggingFilter())
+                .addFilter(new ResponseLoggingFilter())
+                .build();
+    }
+
+    @Test(groups = "specs")
+    public void testGetRequest2() {
+        RestAssured.given()
+                .spec(spec)
+                .when()
+                .get("/authors")
+                .then()
+                .spec(responseSpecificationExample());
+    }
+}
