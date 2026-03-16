e9a79871e7437ba0a1e84ef068c3428b8cb37eac
Santiago Castañón <santiagocastanonarvizu@gmail.com>
2026-03-16 16:38:04 -0600

    feat: add books data provider and test for book POST request in DataSourceTest

--- src/test/java/com/restassured/tests/DataSourceTest.java
+++ src/test/java/com/restassured/tests/DataSourceTest.java
@@ -6,6 +6,7 @@ import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import com.restassured.models.Author;
+import com.restassured.models.Book;
 import com.restassured.utils.ExcelUtils;
 
 import io.restassured.RestAssured;
@@ -14,12 +15,18 @@ import io.restassured.response.Response;
 
 public class DataSourceTest {
 
+    private static final String BOOKS_EXCEL = "src/test/resources/books.xlsx";
     private static final String AUTHORS_EXCEL = "src/test/resources/authors.xlsx";
     private static final String BASE_URL = "http://host.docker.internal:8081/api/v1";
 
     @DataProvider(name = "authorsData")
     public Object[][] authorsData() throws IOException {
         return ExcelUtils.readSheetAsMatrix(AUTHORS_EXCEL, null);
+    }
+
+    @DataProvider(name = "booksData")
+    public Object[][] booksData() throws IOException {
+        return ExcelUtils.readSheetAsMatrix(BOOKS_EXCEL, null);
     }
 
     /*
@@ -50,6 +57,42 @@ public class DataSourceTest {
                 .extract()
                 .response();
 
+        response.prettyPrint();
+    }
+
+    /*
+        POST http://host.docker.internal:8081/api/v1/books HTTP/1.1
+        content-type: application/json
+        Accept: application/json
+        
+        {
+            "title": "The book",
+            "isbn": "12345678",
+            "aisle_number": 1,
+            "author_id": 1
+        }
+    */
+
+    @Test(dataProvider = "booksData")
+    public void testBookPostRequest(String title, String isbn, String authorId, String aisle) {
+
+        Response response = RestAssured.given()
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .baseUri(BASE_URL)
+                .body(Book.builder()
+                        .title(title)
+                        .isbn(isbn)
+                        .authorId(authorId)
+                        .aisle(aisle)
+                        .build())
+                .when()
+                .post("/books")
+                .then()
+                .assertThat()
+                .statusCode(201)
+                .extract()
+                .response();
         response.prettyPrint();
     }
 
